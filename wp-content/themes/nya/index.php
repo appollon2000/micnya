b64dc4dbe895039d81187e591d678fd5b201fed6
michael <michael@eyeballdigital.com>
2014-04-21 15:52:05 -0400

    wordpress paralax theme

--- wp-content/themes/nya/index.php
+++ wp-content/themes/nya/index.php
@@ -3,16 +3,24 @@
 		<div id="content" class="postshome clearfix">
         	<div id="cover-left">&nbsp;</div>
         	<div id="cover-right">&nbsp;</div>
+<ul id="nav"> <!-- post nav -->
+<li id="blockLink1"><a class="block-link" href="#block1">OUR MISSION</a></li>
+<li id="blockLink2"><a class="block-link" href="#block2">HELP US BUILD</a></li>
+<li id="blockLink3"><a class="block-link" href="#block3">THE VIRTUAL SHIMMER WALL</a></li>
+<li id="blockLink4"><a class="block-link" href="#block3">EXPLORE FURTHER</a></li>
 
+</ul> <!--end post nav -->
             <!--end post nav -->
-        	<?php $i = 1; //Start a counter outside of the loop
-        	if(tia_get_option('tia_pages_enabled'))	{
-        		include( TEMPLATEPATH . '/includes/loop-pages.php');
-        	} else {
-						include( TEMPLATEPATH . '/includes/loop-posts.php');
-					}
+        	<div style="background-image: url('/wp-content/uploads/2014/04/volc2.jpg'); overflow: hidden; min-height: 700px; background-repeat: no-repeat; background-position: 50% 0px;" class="post-23 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized parallax-container inview" id="block1">
 
-			wp_reset_postdata(); ?>
+                                         
+  </div>
+  <div style="background: rgba(11, 181, 255, 0.75); overflow: hidden; min-height: 700px; background-repeat: no-repeat; background-position: 50% 0px;" class="post-23 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized parallax-container inview" id="block2">
+
+                                         
+  </div>
+
+			
 
 			<div style="background-color: #777; color:#ccc">
 				<div style="margin:5px; text-align: center">
