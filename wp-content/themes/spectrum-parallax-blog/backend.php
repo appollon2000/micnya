9b2dffaae5a6b8bb4fa9d5dd64e3d7e39364fdea
michael <michael@eyeballdigital.com>
2014-04-29 13:01:58 -0400

    search fcts

--- wp-content/themes/spectrum-parallax-blog/backend.php
+++ wp-content/themes/spectrum-parallax-blog/backend.php
@@ -111,6 +111,220 @@ $mylname. '<br /></h2></div><div class="categz">transformer</div><div class="ts-
 	<div id="results"></div>
 </div>
 
+<div class="clr"></div>
+
+<div id="donateHolder">
+
+<div id="cnvDonationForm">
+    <div id="cnvDonationForm-description">
+        <div id="cnvDonationForm-errors"></div>
+    </div>
+    <table id="CCcheckboxesContainer">
+        <tr><td>Am. Express</td><td>Discover</td><td>MasterCard</td><td>Visa</td></tr>
+    </table>
+    <div id="cnvDonationForm-formContainer">
+        <form id="cnvDonationForm-form" method="post">
+            <input type="hidden" name="method" value="donate" />
+            <input type="hidden" name="v" value="1.0" />
+            <input type="hidden" name="api_key" value="zooapikey" />
+            <input type="hidden" name="source" value="NYA Microsite Donation Form" />
+            <input type="hidden" name="form_id" value="5640" />
+            <input type="hidden" name="level_id" value="8462" />
+            <input type="hidden" name="donor_email_opt_inname" value="implicit" />
+            <input type="hidden" name="donor_email_opt_insubmit" value="true" />
+            <input type="hidden" id="amount" name="other_amount" />
+            <input type="hidden" id="donor_email" name="donor.email" />
+            <div id="info-top">
+                <fieldset id="donationInfo">
+                    <div class="label-field-wrap">
+                        <label class="label" for="card_number">Credit Card Number:</label>
+                        <div class="input1">
+                            <input autocomplete="off" type="number" maxlength="19" id="card_number" class='convi' name="card_number" onkeypress="return App.Helpers.numbersOnly(event,true);" />
+                        </div>
+                    </div>
+                    <div class="label-field-wrap">
+                        <label class="label" for="card_cvv">CVV Number:</label>
+                        <div class="input1">
+                            <input autocomplete="off" type="number" maxlength="4" id="card_cvv" class='convi' name="card_cvv" onkeypress="return App.Helpers.numbersOnly(event,true);" />
+                        </div>
+                    </div>
+                    <div class="label-field-wrap" id="convi-date">
+                        <label class="label" for="card_exp_date_month">Expiration Date:</label>
+                        <div class="dropdown-wrap">
+                            <select id="card_exp_date_month" class='convi-dd' name="card_exp_date_month">
+                                <option></option>
+                                <option value="1">01</option>
+                                <option value="2">02</option>
+                                <option value="3">03</option>
+                                <option value="4">04</option>
+                                <option value="5">05</option>
+                                <option value="6">06</option>
+                                <option value="7">07</option>
+                                <option value="8">08</option>
+                                <option value="9">09</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                            </select>
+                        </div>
+                        <div class="dropdown-wrap">
+
+                            <select id="card_exp_date_year" class='convi-dd' name="card_exp_date_year">
+                                <option></option>
+                                <option value="2013">2014</option>
+                                <option value="2014">2015</option>
+                                <option value="2015">2016</option>
+                                <option value="2016">2017</option>
+                                <option value="2017">2018</option>
+                                <option value="2018">2019</option>
+                                <option value="2020">2019</option>
+                            </select>
+
+                        </div>
+                    </div>
+                    <div id="donationInfo-verisign">
+                    </div>
+                </fieldset>
+            </div>
+            <div id="info-bottom">
+                <fieldset id="yourInfo">
+
+                    <div class="label-field-wrap">
+                        <label class="label" for="billing_name_first">First Name:</label>
+                        <div class="input1">
+                            <input maxlength="50" id="billing_name_first" class='convi' name="billing.name.first" value="" />
+                        </div>
+                    </div>
+                    <div class="label-field-wrap">
+                        <label class="label" for="billing_name_last">Last Name:</label>
+                        <div class="input1">
+                            <input maxlength="50" id="billing_name_last" class='convi' name="billing.name.last" value="" />
+                        </div>
+                    </div>
+                    <div class="label-field-wrap">
+                        <label class="label" for="billing_address_street1">Address:</label>
+                        <div class="input1">
+                            <input maxlength="50" id="billing_address_street1" class='convi' name="billing.address.street1" value="" />
+                        </div>
+                    </div>
+                    <div class="label-field-wrap">
+                        <label class="label" for="billing_address_street2">Address 2 (optional):</label>
+                        <div class="input1">
+                            <input maxlength="50" id="billing_address_street2" name="billing.address.street2" value="" />
+                        </div>
+                    </div>
+                    <div class="label-field-wrap">
+                        <label class="label" for="billing_address_city">City:</label>
+                        <div class="input1">
+                            <input maxlength="30" id="billing_address_city" class='convi' name="billing.address.city" value="" />
+                        </div>
+                    </div>
+                    <div class="label-field-wrap">
+                        <label class="label" for="billing_address_state">State / Province:</label>
+                        <div class="dropdown-wrap">
+                            <select id="billing_address_state" class='convi-dd' name="billing.address.state">
+                                <option></option>
+                                <option value="AK">AK - Alaska</option>
+                                <option value="AL">AL - Alabama</option>
+                                <option value="AR">AR - Arkansas</option>
+                                <option value="AZ">AZ - Arizona</option>
+                                <option value="CA">CA - California</option>
+                                <option value="CO">CO - Colorado</option>
+                                <option value="CT">CT - Connecticut</option>
+                                <option value="DC">DC - District of Columbia</option>
+                                <option value="DE">DE - Delaware</option>
+                                <option value="FL">FL - Florida</option>
+                                <option value="GA">GA - Georgia</option>
+                                <option value="HI">HI - Hawaii</option>
+                                <option value="IA">IA - Iowa</option>
+                                <option value="ID">ID - Idaho</option>
+                                <option value="IL">IL - Illinois</option>
+                                <option value="IN">IN - Indiana</option>
+                                <option value="KS">KS - Kansas</option>
+                                <option value="KY">KY - Kentucky</option>
+                                <option value="LA">LA - Louisiana</option>
+                                <option value="MA">MA - Massachusetts</option>
+                                <option value="MD">MD - Maryland</option>
+                                <option value="ME">ME - Maine</option>
+                                <option value="MI">MI - Michigan</option>
+                                <option value="MN">MN - Minnesota</option>
+                                <option value="MO">MO - Missouri</option>
+                                <option value="MS">MS - Mississippi</option>
+                                <option value="MT">MT - Montana</option>
+                                <option value="NC">NC - North Carolina</option>
+                                <option value="ND">ND - North Dakota</option>
+                                <option value="NE">NE - Nebraska</option>
+                                <option value="NH">NH - New Hampshire</option>  go h
+                                <option value="NJ">NJ - New Jersey</option>
+                                <option value="NM">NM - New Mexico</option>
+                                <option value="NY">NY - New York</option>
+                                <option value="NV">NV - Nevada</option>
+                                <option value="OH">OH - Ohio</option>
+                                <option value="OK">OK - Oklahoma</option>
+                                <option value="OR">OR - Oregon</option>
+                                <option value="PA">PA - Pennsylvania</option>
+                                <option value="RI">RI - Rhode Island</option>
+                                <option value="SC">SC - South Carolina</option>
+                                <option value="SD">SD - South Dakota</option>
+                                <option value="TN">TN - Tennessee</option>
+                                <option value="TX">TX - Texas</option>
+                                <option value="UT">UT - Utah</option>
+                                <option value="VA">VA - Virginia</option>
+                                <option value="VT">VT - Vermont</option>
+                                <option value="WA">WA - Washington</option>
+                                <option value="WI">WI - Wisconsin</option>
+                                <option value="WV">WV - West Virginia</option>
+                                <option value="WY">WY - Wyoming</option>
+                                <option value="AS">AS - American Samoa</option>
+                                <option value="FM">FM - Federated States of Micronesia</option>
+                                <option value="GU">GU - Guam</option>
+                                <option value="MH">MH - Marshall Islands</option>
+                                <option value="MP">MP - Northern Mariana Islands</option>
+                                <option value="PR">PR - Puerto Rico</option>
+                                <option value="PW">PW - Palau</option>
+                                <option value="VI">VI - Virgin Islands</option>
+                                <option value="AA">AA - Armed Forces Americas</option>
+                                <option value="AE">AE - Armed Forces</option>
+                                <option value="AP">AP - Armed Forces Pacific</option>
+                                <option value="AB">AB - Alberta</option>
+                                <option value="BC">BC - British Columbia</option>
+                                <option value="MB">MB - Manitoba</option>
+                                <option value="NB">NB - New Brunswick</option>
+                                <option value="NL">NL - Newfoundland and Labrador</option>
+                                <option value="NS">NS - Nova Scotia</option>
+                                <option value="NT">NT - Northwest Territories</option>
+                                <option value="NU">NU - Nunavut</option>
+                                <option value="ON">ON - Ontario</option>
+                                <option value="PE">PE - Prince Edward Island</option>
+                                <option value="QC">QC - Quebec</option>
+                                <option value="SK">SK - Saskatchewan</option>
+                                <option value="YT">YT - Yukon</option>
+                                <option value="None">None</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="label-field-wrap">
+                        <label class="label" for="billing_address_zip">ZIP / Postal Code:</label>
+                        <div class="input1">
+                            <input id="billing_address_zip" maxlength="10" class='convi' name="billing.address.zip" value="" />
+                        </div>
+                    </div>
+
+
+                </fieldset>
+            </div>
+
+            <div id="submit-wrap">
+                <input type="submit" value="Submit Donation" id="subCvio" />
+            </div>
+        </form>
+    </div>
+    <div id="cnvDonationForm-success"></div>
+</div>
+
+
+</div>
+
 
 
 
