98e04b5761ebd39a04ac392a7efabc65538733a4
michael <michael@eyeballdigital.com>
2014-04-30 10:25:51 -0400

    my javascript

--- wp-content/themes/spectrum-parallax-blog/scripts/global2.js
+++ wp-content/themes/spectrum-parallax-blog/scripts/global2.js
@@ -479,11 +479,61 @@ doct.mouseup(function (e) {
 
 }, 2500);
 
-
+function addWave() {
+var wrid = $('#ib-main-wrapper'),parz = $('div.tyle', wrid);
+  parz.slice(0,36).each(function(i) {
+var row = $(this);
+    setTimeout(function() {
+    row.addClass('skew').animate({opacity:0}).animate({opacity:1}, 100,function() {
+      row.removeClass('skew');
+});
+  }, 300*i);
+});
+parz.slice(36,72).each(function(i) {
+var row = $(this);
+    setTimeout(function() {
+     row.addClass('skew').animate({opacity:0}).animate({opacity:1}, 100,function() {
+      row.removeClass('skew');
+});
+  }, 300*i);
+});
+parz.slice(72,108).each(function(i) {
+var row = $(this);
+    setTimeout(function() {
+    row.addClass('skew').animate({opacity:0}).animate({opacity:1}, 100,function() {
+      row.removeClass('skew');
+});
+  }, 300*i);
+});
+parz.slice(108,144).each(function(i) {
+var row = $(this);
+    setTimeout(function() {
+     row.addClass('skew').animate({opacity:0}).animate({opacity:1}, 100,function() {
+      row.removeClass('skew');
+});
+  }, 300*i);
+});
+parz.slice(144,180).each(function(i) {
+var row = $(this);
+    setTimeout(function() {
+     row.addClass('skew').animate({opacity:0}).animate({opacity:1}, 100,function() {
+      row.removeClass('skew');
+});
+  }, 300*i);
+});
+parz.slice(180,216).each(function(i) {
+var row = $(this);
+    setTimeout(function() {
+     row.addClass('skew').animate({opacity:0}).animate({opacity:1}, 100,function() {
+      row.removeClass('skew');
+});
+  }, 300*i);
+});
+}
 
 //jQuery.fn.reverse = [].reverse;
 //$('.tyle').slice(0,).repeat().each($).animate({opacity:0},$).animate({opacity:1});
-//setInterval(addWave, 14000);
+setInterval(addWave, 14000);
 
 
 
