dabe3384c8eabcb06fb7395879ac609246a541e5
michael <michael@eyeballdigital.com>
2014-05-28 14:30:30 -0400

    css

--- wp-content/themes/spectrum-parallax-blog/personal.php
+++ wp-content/themes/spectrum-parallax-blog/personal.php
@@ -31,7 +31,9 @@ $t_id = $resq3['id'];
 
 $sql4 = "UPDATE dentry SET message = '".mysql_real_escape_string($bodyText)."', icon = '$vicon', recipient_location = '".mysql_real_escape_string($vloc)."', color = '".mysql_real_escape_string($vcolor)."', recipient_first_name = '".mysql_real_escape_string($vname)."', recipient_first_name = '".mysql_real_escape_string($vname)."', recipient_email = '".mysql_real_escape_string($vemail)."', praise_name = '".mysql_real_escape_string($praiseName)."', praise = '$praise' WHERE id = " . $kooks;
 $resq4 = mysql_query($sql4) or die(mysql_error());
-
+if (function_exists('w3tc_pgcache_flush')) {
+    w3tc_pgcache_flush();
+  } 
 }
 
 ?>
