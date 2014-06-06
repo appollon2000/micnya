ac072a493412b43ad0f83bff8b9f69c10fded76d
michael <michael@eyeballdigital.com>
2014-06-06 09:48:38 -0400

    js

--- wp-content/themes/spectrum-parallax-blog/scripts/global2.js
+++ wp-content/themes/spectrum-parallax-blog/scripts/global2.js
@@ -544,28 +544,7 @@ documt.mouseup(function (e) {
 });
 clkDonate.on("click", function(e) {
   e.preventDefault();
-if($('html').hasClass("mozyy")) {
-var trigLink = $('ul#nav li#blockLink5 a'),maindonBtn = $('a#donate-now-button'), isOn = $('#donation-container').is(':visible');
-if (!isOn){
-  alert('on');
-//$("html, body").animate({ scrollTop: $("#donation-container").offset().top });
 
-}else {
-  alert('off');
-  $("html, body").animate({ scrollTop: $("#donation-container").offset().top });
-}
-}else{
-
-var trigLink = $('ul#nav li#blockLink5 a'),maindonBtn = $('a#donate-now-button'), isOn = $('#donation-container').is(':visible');
-if (!isOn){
-$("html, body").animate({ scrollTop: $("#block5").offset().top });
-setTimeout(function () {
-maindonBtn.trigger('click');
-}, 1400);
-}else {
-  $("html, body").animate({ scrollTop: $("#block5").offset().top });
-}
-}
 });
 
 clkArrow.on("click", function(e) {
