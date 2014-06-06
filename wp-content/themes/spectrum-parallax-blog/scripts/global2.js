742ba90bac1839e316b770f8e8aad4199f248a14
michael <michael@eyeballdigital.com>
2014-06-06 09:46:10 -0400

    js

--- wp-content/themes/spectrum-parallax-blog/scripts/global2.js
+++ wp-content/themes/spectrum-parallax-blog/scripts/global2.js
@@ -549,9 +549,7 @@ var trigLink = $('ul#nav li#blockLink5 a'),maindonBtn = $('a#donate-now-button')
 if (!isOn){
   alert('on');
 $("html, body").animate({ scrollTop: $("#donation-container").offset().top });
-setTimeout(function () {
-maindonBtn.trigger('click');
-}, 1400);
+
 }else {
   alert('off');
   $("html, body").animate({ scrollTop: $("#donation-container").offset().top });
